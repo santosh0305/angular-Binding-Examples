26aa362d078a4d26a7ded1e3b2bbcc912ae19455
santosh0305 <2mymail.santosh@gmail.com>
2020-07-16 22:49:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-djucfj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-djucfj)
---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + JSON.stringify(VERSION)
+  age : number = 37
+  imageUrl : string = "https://picsum.photos/seed/picsum/200/300"
+
+  states = [
+      {
+        "id": 1,
+        "name": "TG"
+      },
+      {
+        "id": 2,
+        "name": "AP"
+      }
+    ]
+
+  SaveUsers(){
+    console.log('i am clicked !!')
+  }
+
+  OnChange(event : any){
+    console.log("Some change happened")
+    console.log(event.target.value);
+  }
+
+}
